484764e486556f9a3ca21b6837ec4761911cd13e
Justin Bradley <bradley.justin@gmail.com>
2022-01-27 19:49:18 -0600

    Fixed weird whitespace issue.
     *     Justin: the original parser didn't properly tokenize when there were no
     *     spaces around the operators. This was fine in the tests because we never
     *     tested for that case, but it was not robust for student testing. To fix
     *     this, we now just put spaces around all operators before the tokenizer.

--- parser.cpp
+++ parser.cpp
@@ -16,10 +16,16 @@
  * Updates:
  * 2022-01-23 Fixes to handle no spaces after semicolons (courtesy of Jaden
  * Goter)
+ * 2022-01-27 Improved operator handling. Now no weird whitespace issues
+ *     Justin: the original parser didn't properly tokenize when there were no
+ *     spaces around the operators. This was fine in the tests because we never
+ *     tested for that case, but it was not robust for student testing. To fix
+ *     this, we now just put spaces around all operators before the tokenizer.
  * 
  */
 
 #include <sstream>
+#include <regex>
 
 #include "parser.hpp"
 
@@ -76,25 +82,41 @@ std::vector<shell_command> parse_command_string(const std::string& str)
         need_out_path
     } state = parser_state::need_new_command;
 
-	// To handle spaces after semicolons we preprocess the string and add a
-	// space after each semicolon. This allows std::istringstream to easily
-	// tokenize based on spaces. (credit: Jaden Goter)
+	// put spaces around operators > < >> && || ; |
     std::string new_str;
-    for (int i = 0; i < (int)str.length() - 1; i++) {
-        new_str += str[i];
-        if (str[i] == ';' && (i+1)<((int)str.length()-1) && str[i + 1] != ' ') {
-            new_str += ' ';
-        }
+    for (int i = 0; i < (int)str.length(); i++) {
+		// first look for && || and >>
+        if ((str[i] == '>' && str[i+1] == '>') ||
+			(str[i] == '&' && str[i+1] == '&') ||
+			(str[i] == '|' && str[i+1] == '|')){
+				new_str += std::string(" ")+str[i]+str[i+1]+" ";
+				i++;
+				// std::cout << "Found >> && or ||" << std::endl;
+		}
+		// next look for > < ; |
+		else if(str[i] == ';' ||
+				str[i] == '>' ||
+				str[i] == '<' ||
+				str[i] == '|') {
+				new_str += std::string(" ")+str[i]+" ";
+				// std::cout << "Found ; > < |" << std::endl;
+		}
+		// no operator, so just add the character
+		else {
+				new_str += str[i];
+		}
     }
-    new_str += str[str.length() - 1];
-	// end new code
+	// std::cout << "str: " << str << std::endl;
+	// std::cout << "new_str: " << new_str << std::endl;		
 
+	// Now parse and iterate over the tokens
 	std::istringstream iss(new_str);
     std::string token;
     while (iss >> token) {
         auto token_type = get_shell_token_type(token);
+		// std::cout << "Beginning of while state is: " << (int) state << std::endl;
 
-        switch (state) {
+        switch (state) {				
         case parser_state::need_any_token:
             switch (token_type) {
             case shell_token_type::text:
@@ -158,20 +180,24 @@ std::vector<shell_command> parse_command_string(const std::string& str)
 
         case parser_state::need_in_path:
             if (token_type != shell_token_type::text) {
-                throw parsing_error("expecting an input path");
+                throw parsing_error("Expecting an input path");
             }
             commands.back().cin_file = token;
             state = parser_state::need_any_token;
             break;
 
         case parser_state::need_out_path:
+				// std::cout << "in need_out_path" << std::endl;
+				
             if (token_type != shell_token_type::text) {
-                throw parsing_error("expecting an output path");
+                throw parsing_error("Expecting an output path");
             }
             commands.back().cout_file = token;
             state = parser_state::need_any_token;
             break;
         }
+		// int s = (int) state;
+		// std::cout << "End of while state is: " << s << std::endl;
     }
 
     if (commands.back().cmd == "") {
